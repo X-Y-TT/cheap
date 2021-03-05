84ff8d41af0624e5480e5729a6438e9363d8681b
U188 <54225629+U188@users.noreply.github.com>
2021-03-05 10:04:38 +0800

    Update service.py

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 import cheap,tizi
 from  flask import Flask,jsonify,request
 app = Flask(__name__)
+'''
 @app.route("/ssr",methods=["GET"])
 def cheap1():
     ssr=cheap.main()
@@ -12,11 +13,19 @@ def cheap1():
 def tizi1():
     ssr=tizi.main()
     return ssr
+'''
 @app.route("/tizi/<invite>",methods=["GET"])
 def tizi2(invite):
-    invite = 'https://www.zionladdero.com/register/{}'.format(invite)
-    tizi.main2(invite)
-    return jsonify({'code' :200, 'msg': 'ok'})
+    with open("invite.txt", "r") as f:
+        s = f.readlines()
+        invite = 'https://www.zionladdero.com/register/{}'.format(invite)+'\n'
+        if invite in s:
+            return jsonify({'code': 404, 'msg': '错误！'})
+        else:
+            f = open('invite.txt', 'a')
+            f.write(invitecodes)
+            f.close()
+            return jsonify({'code' :200, 'msg': ssr})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5008, debug=True)
